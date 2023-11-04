426fe6338a3474b92fdd08b86036de6aff9afbd2
Bayode Aderinola <bayodesegun@gmail.com>
2023-11-04 22:23:22 +0100

    Exercise 4.9: Blog list tests, step2: test that each blog has a unique id attribute

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -6,23 +6,42 @@ const api = supertest(app)
 const listHelper = require('../utils/list_helper')
 const Blog = require('../models/blog')
 
+// Get three out of the test blogs
+const testBlogs = listHelper.allBlogs.slice(0, 3)
 
-beforeEach(async () => {
+beforeAll(async () => {
   await Blog.deleteMany({})
-  for (let blog of listHelper.allBlogs.slice(0, 3)) {
+  for (let blog of testBlogs) {
     const blog_ = Blog(blog)
     await blog_.save()
   }
 }, 100000)
 
-test('blogs are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('blogs list', () => {
+  test('returns list as json with code 200 OK', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(3)
-}, 100000)
+  }, 100000)
+
+  test('returns the right number of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(3)
+  }, 100000)
+
+  test('returns blogs with unique IDs named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    let prevId = 'someRandomId'
+    for (let blog of blogs) {
+      expect(blog.id).toBeDefined()
+      expect(blog.id).not.toBe(prevId)
+      prevId = blog.id
+    }
+  }, 100000)
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
